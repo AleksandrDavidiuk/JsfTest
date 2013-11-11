7e72566768798a99442b19c23590766145eafa7f
unknown <aleksandr.davidiuk@LAB9.engagepoint-lab.local>
2013-11-11 17:49:44 +0200

    First commit

---
+++ src/main/java/HelloBean.java
@@ -0,0 +1,31 @@
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aleksandr.davidiuk
+ * Date: 11/8/13
+ * Time: 2:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@ManagedBean(name = "hello")
+@ViewScoped
+public class HelloBean implements Serializable {
+
+    private String message = "Hello SANYA";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void test() {
+        this.message = "durak";
+    }
+}
